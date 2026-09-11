f0fc6da1b39eeb58c2994d74fa298d984d781bd9
pavnigupta1301-sketch <pavnigupta1301@gmail.com>
2026-09-11 23:13:41 +0530

    Add files via upload

---
+++ a5.java
@@ -0,0 +1,39 @@
+public class a5 {
+    
+
+    static void classifyWordLengths(String review) {
+
+        int shortCount = 0;
+        int mediumCount = 0;
+        int longCount = 0;
+
+        String[] words = review.split(" ");
+
+        for (int i = 0; i < words.length; i++) {
+
+            int length = words[i].length();
+
+            if (length >= 1 && length <= 4) {
+                shortCount++;
+            }
+            else if (length >= 5 && length <= 8) {
+                mediumCount++;
+            }
+            else if (length >= 9) {
+                longCount++;
+            }
+        }
+
+        System.out.println("Short: " + shortCount);
+        System.out.println("Medium: " + mediumCount);
+        System.out.println("Long: " + longCount);
+    }
+
+    public static void main(String[] args) {
+
+        String review = "This movie was absolutely fantastic and thrilling";
+
+        classifyWordLengths(review);
+    }
+
+}
