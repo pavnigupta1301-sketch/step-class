ee4ef5429ff8a3829726d701400377764fdf2fd8
pavnigupta1301-sketch <pavnigupta1301@gmail.com>
2026-09-18 23:03:56 +0530

    Add files via upload

---
+++ s6a5.java
@@ -0,0 +1,32 @@
+class Employee {
+    String empName;
+    double salary;
+
+    static String companyName = "Bright Horizon Technologies";
+    static int employeeCount = 0;
+
+    Employee(String empName, double salary) {
+        this.empName = empName;
+        this.salary = salary;
+
+        employeeCount++;
+    }
+
+    static void printCompanyInfo() {
+        System.out.println(companyName);
+        System.out.println("Employees on record: " + employeeCount);
+    }
+}
+
+public class s6a5 {
+    public static void main(String[] args) {
+
+        Employee employee1 = new Employee("Ravi", 50000);
+        Employee employee2 = new Employee("Priya", 60000);
+        Employee employee3 = new Employee("Arjun", 45000);
+
+        System.out.println("3 Employee objects created");
+
+        Employee.printCompanyInfo();
+    }
+}
